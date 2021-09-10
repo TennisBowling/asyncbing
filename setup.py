cfe60fcb686e5d2f26a3611a6948e0be0594cec8
TennisBowling <33209196+TennisBowling@users.noreply.github.com>
2021-09-10 09:21:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='asyncbing',
-    version='0.1.0',
+    version='1.0.0',
     author='TennisBowling',
     author_email='tennisbowling@tennisbowling.com',
     packages=['asyncbing'],
