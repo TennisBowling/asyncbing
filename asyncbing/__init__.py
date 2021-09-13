5013d1b9752eecad1ce1d380b65ff28d06e56d6a
TennisBowling <tennisbowling@tennisbowling.com>
2021-09-12 19:11:09 -0700

    bump readthedocs

--- asyncbing/__init__.py
+++ asyncbing/__init__.py
@@ -2,12 +2,3 @@ from .bingresponse import *
 from .exceptions import *
 from .search import *
 from .searchresult import *
-
-#__all__ = ['Search', 'BingSearchResult', 'BingResponse', 'BingException', 'BadAuth', 'NotEnoughResults']
-
-#Search.__module__ = 'asyncbing'
-#BingSearchResult.__module__ = 'asyncbing'
-#BingResponse.__module__ = 'asyncbing'
-#BingException.__module__ = 'asyncbing'
-#BadAuth.__module__ = 'asyncbing'
-#NotEnoughResults.__module__ = 'asyncbing'
