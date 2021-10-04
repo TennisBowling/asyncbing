ed180b8781cb195f7718b9cb89e5de17e7c909bd
TennisBowling <33209196+TennisBowling@users.noreply.github.com>
2021-10-04 10:26:32 -0700

    docs bugfix

--- asyncbing/searchresult.py
+++ asyncbing/searchresult.py
@@ -1,5 +1,6 @@
 class SearchResult:
     """Represents a Bing Search Result (like url, snippet, name, etc)
+    
     .. warning::
         You must not manually initialize this!"""
     def __init__(self, data):
